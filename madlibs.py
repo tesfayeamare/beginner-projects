d7a976037691b9f69dce126a66dcf26c6f8abeb4
tesfaye <tesfaye.naramo@gmail.com>
2022-06-16 21:01:56 +0200

    read me

--- madlibs.py
+++ madlibs.py
@@ -1,17 +1,7 @@
 
 def madlib():
-
     
-    ''' Mad Libs is a phrasal template word game created by Leonard Stern and Roger Price.
-    It consists of one player prompting others for a list of words to substitute for 
-    blanks in a story before reading aloud.
-    The game is frequently played as a party game or as a pastime.'''
-
-
-
-
-    from turtle import color
-    print(" Welcome to Cutting-Edge Technology Madlib Game")
+    print(" Welcome to Cutting-Edge of Technology Madlib Game")
     print("Put your guess in the next to the given world ")
     print(" Enjoy the game")
 
